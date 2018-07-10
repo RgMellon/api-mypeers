554c64d128b6bafc293e644422fb9fc288ab0c07
Renan <renan_gian@hotmail.com>
2018-07-10 08:27:16 -0300

    tirando resize

--- app/ImgUpload.php
+++ app/ImgUpload.php
@@ -15,7 +15,7 @@ class ImgUpload extends Model
         .explode('/', explode(':', substr($imageData, 0, strpos($imageData,';')))[1])[1];
 
         Image::cache(function($image) {
-            $image->make($this->imgD)->resize(250, 250);
+            $image->make($this->imgD);
         },10, true)->save(public_path('images/prod/').$fileName, 60);
 
         return $fileName;
