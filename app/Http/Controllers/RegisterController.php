696518ed3aff1b8ccd1cb715226f157c1a3b20a1
Renan <renan_gian@hotmail.com>
2018-07-17 07:52:42 -0300

    add register

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -6,7 +6,41 @@ use Illuminate\Http\Request;
 
 class RegisterController extends Controller
 {
-    public function register() {
-        return 'oi';
+    public function register(Request $request) {
+        $v = validator($request->only('email', 'name', 'password'), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:6',
+        ]);
+
+        if ($v->fails()) {
+            return response()->json($v->errors()->all(), 400);
+        }
+        $data = request()->only('email','name','password');
+
+        $user = \App\User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => bcrypt($data['password']),
+        ]);
+
+        $client = \Laravel\Passport\Client::where('password_client', 1)->first();
+
+        $request->request->add([
+            'grant_type' =>'password',
+            'client_id' =>'3',
+            'client_secret' =>'D0ymFxTPiplIRomp2LpO6IB1ILjUBbRg6h0JCc3H',
+            'username' => $request->get('email'),
+            'password' => $request->get('password'),
+            'scope' => null,
+        ]);
+        
+        // Fire off the internal request. 
+        $proxy = Request::create(
+            'oauth/token',
+            'POST'
+        );
+
+        return \Route::dispatch($proxy);
     }
 }
