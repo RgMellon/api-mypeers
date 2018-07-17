fc89aee2b46ffe94e430998fab6167f614a9b22d
Renan <renan_gian@hotmail.com>
2018-07-17 08:00:28 -0300

    change register

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -24,7 +24,7 @@ class RegisterController extends Controller
             'password' => bcrypt($data['password']),
         ]);
 
-        $client = \Laravel\Passport\Client::where('password_client', 1)->first();
+        
 
         $request->request->add([
             'grant_type' =>'password',
